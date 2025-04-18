071246b7057257bc84a9792379ca3f2ff6fe15e5
Surya Selvakumar <surya@Suryas-MacBook-Air.local>
2025-04-18 12:24:16 +0100

    keyword verify - tqdm update; tqdm addition to requirements

--- keyword_verify.py
+++ keyword_verify.py
@@ -1,5 +1,6 @@
 import json 
 import os
+from tqdm import tqdm
 
 def string_match(keyword, statement):
     return keyword.lower() in statement.lower()
@@ -8,7 +9,7 @@ def verify_keyword_occurence(data):
 
     unrelated = {}
     
-    for keyword, posts in data.items():
+    for keyword, posts in tqdm(data.items(), desc='Checking Keywords Mismatch: '):
         unrelated[keyword] = []
         for post in posts:
             if keyword.lower() not in post['title'].lower():
@@ -18,7 +19,7 @@ def verify_keyword_occurence(data):
 
 
 if __name__=='__main__':
-    filename = os.path.join('data', 'ukpolitics_data.json')
+    filename = os.path.join('data', 'ukpolitics_4.json')
     with open(filename, 'r') as json_file:
         data = json.load(json_file)
 
